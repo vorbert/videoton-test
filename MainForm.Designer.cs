f4c1354ae1c3046b9e8d495b35dffa21e9f87a94
Peter Voros <vorbert67@gmail.com>
2025-06-04 09:59:32 +0200

    initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace VideotonTestApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MsgTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.goBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MsgTextBox
+            // 
+            this.MsgTextBox.Location = new System.Drawing.Point(12, 48);
+            this.MsgTextBox.MaxLength = 500;
+            this.MsgTextBox.Multiline = true;
+            this.MsgTextBox.Name = "MsgTextBox";
+            this.MsgTextBox.ReadOnly = true;
+            this.MsgTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.MsgTextBox.Size = new System.Drawing.Size(413, 169);
+            this.MsgTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(94, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Code Logger";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // goBtn
+            // 
+            this.goBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.goBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goBtn.Location = new System.Drawing.Point(12, 239);
+            this.goBtn.Name = "goBtn";
+            this.goBtn.Size = new System.Drawing.Size(413, 44);
+            this.goBtn.TabIndex = 2;
+            this.goBtn.Text = "GO";
+            this.goBtn.UseVisualStyleBackColor = true;
+            this.goBtn.Click += new System.EventHandler(this.goBtn_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.stopBtn.Enabled = false;
+            this.stopBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopBtn.Location = new System.Drawing.Point(12, 289);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(413, 44);
+            this.stopBtn.TabIndex = 3;
+            this.stopBtn.Text = "STOP";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveBtn.Enabled = false;
+            this.saveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveBtn.Location = new System.Drawing.Point(12, 339);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(413, 44);
+            this.saveBtn.TabIndex = 4;
+            this.saveBtn.Text = "ADAT MENTÉS";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(437, 450);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.goBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MsgTextBox);
+            this.Name = "MainForm";
+            this.Text = "VideotonTest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox MsgTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button goBtn;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.Button saveBtn;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static GetID.GetID;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace VideotonTestApp
+{
+    public partial class MainForm : Form
+    {
+        delegate void SetTextCallback(string text, bool isError);
+        private Thread formThread = null;
+
+        GetID.GetID getId = new GetID.GetID();
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            getId.ValueChanged += GetIdValueChanged;
+            getId.ErrorChanged += GetIdErrorChanged;
+        }
+
+        /**
+         * Handling OnErrorChanged event in GetID class
+         */
+        private void GetIdErrorChanged(object sender, PropertyChangedEventArgs e)
+        {
+            // thread is added so I could access the Ui elements. Pls see further down!
+            this.formThread = new Thread(new ThreadStart(this.addErrorToTextBox));
+
+            this.formThread.Start();
+        }
+
+        /**
+         * Handling OnValueChanged event in GetID class
+         */
+        private void GetIdValueChanged(object sender, PropertyChangedEventArgs e)
+        {
+            // thread is added so I could access the Ui elements. Pls see further down!
+            this.formThread =  new Thread(new ThreadStart(this.addToTextBox));
+
+            this.formThread.Start();
+        }
+
+        /**
+         * Go button click event handling
+         */
+        private void goBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(MsgTextBox.Text.Length > 0)
+                {
+                    MsgTextBox.Clear();
+                    saveBtn.Enabled = false;
+                }
+
+                getId.Go();
+                goBtn.Enabled = false;
+                stopBtn.Enabled = true;
+
+                if (getId.Running)
+                {
+                    MsgTextBox.AppendText("A GetId szolgáltatás elindult!\r\n");
+                }
+                else
+                {
+                    MsgTextBox.AppendText("A GetId szolgáltatás nem fut! Kérjük lépjen kapcsolatba az adminisztrátorral!\r\n");
+                    getId.Stop();
+                    goBtn.Enabled = true;
+                    stopBtn.Enabled = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                getId.Stop();
+                goBtn.Enabled = true;
+                stopBtn.Enabled = false;
+                MsgTextBox.AppendText($"{ex.Message}\r\n");
+            }
+        }
+
+        /**
+         * Sopt button click event handlig
+         */
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            this.stopGetIdRunning();
+        }
+
+        /**
+         * created a reusable stop method
+         */
+        private void stopGetIdRunning()
+        {
+            getId.Stop();
+
+            if (!getId.Running && stopBtn.Enabled)
+            {
+                MsgTextBox.AppendText("A GetId szolgáltatás leállt!\r\n");
+                goBtn.Enabled = true;
+                stopBtn.Enabled = false;
+                saveBtn.Enabled = true;
+            }
+            else
+            {
+                MsgTextBox.AppendText("A GetId szolgáltatást nem sikerült leállítani! Kérjük lépjen kapcsolatba az adminisztrátorral!\r\n");
+            }
+        }
+
+        /**
+         * Handle the error text, and push it to the UI textbox element
+         */
+        private void addErrorToTextBox()
+        {
+            this.setText($"A GetId szolgáltatásban hiba lépett fel! Kérjük lépjen kapcsolatba az adminisztrátorral! \r\nHibakód: {getId.ErrorMessage}\r\n", false);
+        }
+
+
+        /**
+         * Do the string quality verdict, also push the text to the UI textbox element
+         */
+        private void addToTextBox()
+        {
+            string pattern = @"(^[A-Z]{1})([0-9]{4})";
+            string verdict;
+
+            if(Regex.IsMatch(getId.Value, pattern))
+            {
+                verdict= "- MEGFELELŐ";
+            } 
+            else
+            {
+                verdict = "- NEM MEGFELELŐ";
+            }
+
+            this.setText($"{getId.Value} {verdict}\r\n", true);
+        }
+
+        /**
+         * I added this method here so it could be called back, if needed. I did this, because I couldn't acces the UI elements 
+         */
+        private void setText(string text, bool isError)
+        {
+            if (this.MsgTextBox.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(setText);
+                this.Invoke(d, new object[] { text, isError });
+            }
+            else
+            {
+                if(getId.Running)
+                {
+                    this.MsgTextBox.Text += text;
+                    this.MsgTextBox.SelectionStart = this.MsgTextBox.Text.Length;
+
+                    // hadling stop on error, or if we run out of storage space
+                    if (this.MsgTextBox.Text.Length >= (this.MsgTextBox.MaxLength - 200) || !isError)
+                    {
+                        this.stopGetIdRunning();
+                    }
+                }                
+            }
+        }
+
+        /**
+         * Handleled save data to file button and saved string value from the MsgTextBox to .xtx file
+         */
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(MsgTextBox.Text.Length > 0)
+                {
+                    SaveFileDialog file = new SaveFileDialog();
+                    file.FileName = "codes.txt";
+                    file.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                    file.ShowDialog();
+
+                    using (StreamWriter filewrite = new StreamWriter(file.FileName))
+                    {
+                        using (StringReader reader = new StringReader(MsgTextBox.Text))
+                        {
+                            string line;
+                            while ((line = reader.ReadLine()) != null)
+                            {
+                                filewrite.WriteLine(line);
+                            }
+                            MessageBox.Show("Adatmentés sikeres!");
+                        }
+                    }   
+                } 
+                else
+                {
+                    MessageBox.Show("Hiba! Nincs elég adat a mentéshez!");
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show($"Adat mentés közben hiba történt. \r\nError: {ex.Message}");
+            }
+        }
+    }
+}
