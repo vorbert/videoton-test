9c9d23dccc6fef703c8e642ecd7cf03f7f8eaf02
Peter Voros <vorbert67@gmail.com>
2025-06-04 12:00:37 +0200

    added a simple loggin function to the project

---
+++ SimpleLogging.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace VideotonTestApp
+{
+
+    public class SimpleLogging
+    {
+        private readonly ILogger _logger;
+
+        public SimpleLogging(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void itLogsError(string msg)
+        {
+            _logger.LogError("ERROR OCURED: {message}", msg);
+        }
+
+        public void itLogsInfo(string msg)
+        {
+            _logger.LogInformation("INFO: {message}", msg);
+        }
+
+    }
+}
